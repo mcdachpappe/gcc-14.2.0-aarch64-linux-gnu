d99ea8dc35724d22864613f98dcc6a2eefc77aaf
mcdachpappe <noreference@web.de>
2025-04-05 23:58:25 +0200

    TC: Import of minimal GCC 14.2.0
    BINUTILS_VERSION=2.43
    GCC_VERSION=14.2.0
    
    Signed-off-by: mcdachpappe <noreference@web.de>

---
+++ lib/gcc/aarch64-linux-gnu/14.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/mcd/cross-kernel/src/gcc-14.2.0/configure --target=aarch64-linux-gnu --prefix=/home/mcd/cross-kernel/tools --without-headers --with-newlib --disable-nls --disable-shared --disable-threads --disable-multilib --disable-libssp --disable-libquadmath --disable-libgomp --disable-libvtv --disable-libstdcxx --disable-libatomic --disable-libsanitizer --disable-libitm --enable-languages=c";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
