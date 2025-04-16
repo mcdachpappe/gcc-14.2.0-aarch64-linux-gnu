d330beeb61a7b6357a07b10cd94f2848b1e1e79c
mcdachpappe <noreference@web.de>
2025-04-16 22:16:07 +0200

    tc: Import minimal GCC 14.2.0
    BINUTILS_VER=2.43
    GCC_VER=14.2.0
    TARGET=aarch64-linux-gnu
    
    Signed-off-by: mcdachpappe <noreference@web.de>

---
+++ lib/gcc/aarch64-linux-gnu/14.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/mcd/android/build-gcc/src/gcc-14.2.0/configure --target=aarch64-linux-gnu --prefix=/home/mcd/android/gcc-14.2.0 --with-build-sysroot=/home/mcd/android/gcc-14.2.0 --without-headers --disable-nls --disable-multilib --disable-libatomic --disable-libssp --disable-libquadmath --disable-libgomp --disable-libvtv --disable-libsanitizer --disable-shared --enable-languages=c";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
